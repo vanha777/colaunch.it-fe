9a30e7f97634a2337cae9a38d1310447c16f7c5f
vanha777 <vanha777@icloud.com>
2025-02-28 10:59:37 +1100

    x

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -59,7 +59,7 @@ export default function NavBar() {
                     {/* CTA Button */}
                     <div>
                         <Link
-                            href="/signup"
+                            href="/dashboard"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="bg-gradient-to-r from-blue-500 to-purple-500 text-white px-6 py-2 rounded-lg font-medium 
